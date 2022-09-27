8608b6a53a77a992f596862040c9f6db0080ca24
AlexandraKrosnik <alexandra.krosnik@gmail.com>
2022-09-27 13:32:49 +0300

    Fixed statistics

--- controllers/statistics/add.js
+++ controllers/statistics/add.js
@@ -1,44 +1,37 @@
 const e = require("express");
-const { BadRequest, NotFound } = require("http-errors");
+const { BadRequest, NotFound, Locked } = require("http-errors");
 const { books } = require("..");
 const { Training, Book, Statistic } = require("../../models");
 
-const add = async (req, res) => {
-  const { id } = req.user;
-  const { statisticId, newStatistic } = req.body;
-  const statistic = await Statistic.findOne({ _id: statisticId });
-  if (!statistic) {
-    throw NotFound(`Statistic with id=${statisticId} not found!`);
-  }
-  const isDateAdded = statistic.result.find(
-    (res) => res.date === new Date(newStatistic.date)
+const presentDate = (result, newDate) =>
+  result.find(
+    (res) =>
+      res.date.getDate() === new Date(newDate).getDate() &&
+      res.date.getMonth() === new Date(newDate).getMonth() &&
+      res.date
   );
+const updateResult = (oldResult, newStatistic, dateAdded) => {
   let result;
-  if (!isDateAdded) {
-    result = [...statistic.result, { ...newStatistic }];
+  if (!dateAdded) {
+    result = [...oldResult, { ...newStatistic }];
   } else {
-    result = statistic.result.map(({ date, pages }) => {
-      if (date === new Date(newStatistic.date)) {
+    result = oldResult.map(({ date, pages }) => {
+      if (
+        date.getDate() === new Date(newStatistic.date).getDate() &&
+        date.getMonth() === new Date(newStatistic.date).getMonth()
+      ) {
         return { date, pages: pages + newStatistic.pages };
       }
       return { date, pages };
     });
   }
-  console.log(result);
-  const pagesAmount = result.reduce((sum, { pages }) => sum + pages, 0);
+  return result;
+};
+const sumPagesToRead = (result) =>
+  result.reduce((sum, { pages }) => sum + pages, 0);
 
-  const training = await Training.findOne({
-    user: id,
-    statistics: statisticId,
-  });
-  // .populate({ path: "books.book", model: "book" })
-  // .populate({ path: "statistics", model: "statistic" });
-  if (!training) {
-    throw BadRequest(`Check the entered data(id-${statisticId}!`);
-  }
-  const { books } = training;
-  const changeTraining = books;
-  changeTraining.reduce((pages, infBook, index, arr) => {
+const changeTrainingBooksList = (books, pagesAmount) => {
+  const leftPages = books.reduce((pages, infBook, index, arr) => {
     if (pages === 0) {
       return pages;
     } else if (infBook.leftPages === 0) {
@@ -62,37 +55,64 @@ const add = async (req, res) => {
     return pages;
   }, pagesAmount);
 
-  const statusUpdate = changeTraining.map((books) => {
-    if (books.status) {
-      return books.book;
-    }
-    return false;
+  return { leftPages, books };
+};
+
+const add = async (req, res) => {
+  const { id } = req.user;
+  const { statisticId, newStatistic } = req.body;
+  const statistic = await Statistic.findOne({ _id: statisticId });
+  if (!statistic) {
+    throw NotFound(`Statistic with id=${statisticId} not found!`);
+  }
+  const dateAdded = presentDate(statistic.result, newStatistic.date);
+  const newResult = updateResult(statistic.result, newStatistic, dateAdded);
+  console.log(newResult);
+
+  const training = await Training.findOne({
+    user: id,
+    statistics: statisticId,
   });
+  // .populate({ path: "books.book", model: "book" })
+  // .populate({ path: "statistics", model: "statistic" });
+  if (!training) {
+    throw BadRequest(`Check the entered data(id-${statisticId}!`);
+  }
+  const { books } = training;
+
+  const { leftPages, books: trainingListBook } = changeTrainingBooksList(
+    books,
+    newStatistic.pages
+  );
+  if (leftPages > 0) {
+    throw Locked("All books have been already read");
+  }
+  const booksStatusUpdate = trainingListBook.filter(
+    (books) => books.status && books.book
+  );
+  const leftBooks = statistic.leftBooks - booksStatusUpdate.length;
 
   const updateStatistic = await Statistic.findByIdAndUpdate(
     { _id: statisticId },
-    { result },
+    { result: newResult, leftBooks },
     { new: true }
   );
   const updateTraining = await Training.findByIdAndUpdate(
     { _id: training._id.toString() },
-    { books: changeTraining },
+    { books: trainingListBook },
     { new: true }
   );
 
-  statusUpdate.map(async (status) => {
-    if (!status) {
-      return status;
-    }
-    const book = await Book.findByIdAndUpdate(
-      { _id: status, user: id },
+  booksStatusUpdate.map(async ({ book }) => {
+    const bookUpdate = await Book.findByIdAndUpdate(
+      { _id: book, user: id },
       { status: "already" },
       { new: true }
     );
-    if (!book) {
+    if (!bookUpdate) {
       throw new Error(`Book's status has not been updated!`);
     }
-    return book;
+    return bookUpdate;
   });
   res.status(200).json({
     message: "Success",
